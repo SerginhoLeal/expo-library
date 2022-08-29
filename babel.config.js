f866a855a005b1236fa6a12dfe0cd4fde82c80be
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-08-29 19:53:45 -0300

    remove reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,6 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       'babel-plugin-styled-components',
-      'react-native-reanimated/plugin',
       [
         'module-resolver',
         {
