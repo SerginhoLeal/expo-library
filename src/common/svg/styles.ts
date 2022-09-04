7d9b05cff1339d221d25da62c7180d7eb39100e2
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-04 11:24:57 -0300

    adding new icon

---
+++ src/common/svg/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+import Svg, { Path } from 'react-native-svg';
+
+export const StyledSvg = styled(Svg)``;
+export const StyledPath = styled(Path)``;
---
+++ src/common/svg/types.ts
@@ -0,0 +1,5 @@
+export type Props = {
+  name: 'home' | 'user' | 'download' | 'plus' | 'arrow-rotate' | 'check' | 'play' | 'pause' | 'google-photo' | 'loading';
+  size?: string;
+  color?: string;
+};
