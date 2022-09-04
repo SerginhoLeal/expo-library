f711ef8c2de64f3cf8f6aff4750e7dd2cfa77163
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-04 11:21:10 -0300

    refactor home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,75 +1,40 @@
 import * as React from 'react';
 import * as Styles from './styles';
-import * as Native from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import * as MediaLibrary from 'expo-media-library';
-
-import Modal from 'react-native-modal';
-
-import { Masonry, ModalScreen } from '@common';
 
 import { useContext } from '@context';
 
+import { Masonry, ModalScreen } from '@common';
+
 const Home: React.FC = () => {
   const { media } = useContext();
-  
-  const [progress, setProgress] = React.useState<number>(0);
-  const [state, setState] = React.useState({ id: 0, downloaded: false, url:'', creator:'', modal:false, type:'', index: 0 });
 
-  const handleSelectItem = (item: any) => {
-    setState({
-      ...state,
-      id: item.id,
-      modal: true,
-      creator: `${item.id}_${item.createby}`,
-      url: item.url,
-      type: item.type,
-      downloaded: item.downloaded,
-      index: item.index,
-    });
-    setProgress(0)
-  };
+  const [state, setState] = React.useState({
+    id:0,
+    url:'',
+    mediaType:'',
+    index:0,
+    screen: '',
+    creator:'',
+    modal:false,
+    downloaded:false,
+  });
 
-  const callback = (downloadProgress: FileSystem.DownloadProgressData) => {
-    const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
-    setProgress(progress);
-  };
-
-  const handleDownload = async (url: string) => {
-    const downloadResumable: FileSystem.DownloadResumable = FileSystem.createDownloadResumable(
-      url,
-      `${FileSystem.documentDirectory}${state.creator}${state.type === 'video' ? '.mp4' : '.png'}`,
-      {},
-      callback
-    );
-
-    const album = await MediaLibrary.getAlbumAsync('Storage');
-
-    const { uri, status }: any = await downloadResumable.downloadAsync();
-    const asset = await MediaLibrary.createAssetAsync(uri);
-
-    if(status === 200){};
-
-    try {
-      if (album == null) {
-        await MediaLibrary.createAlbumAsync('Storage', {...asset}, false);
-      } else {
-        await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
-      };
-    } catch (e) {};
-  };
+  const handleSelectItem = (item: any) => setState({
+    ...state,
+    ...item,
+    modal: true,
+    screen: 'data',
+    creator: `${item.id}_${item.createby}`,
+  });
 
   return (
     <Styles.Container>
-      <Masonry data={[media]} handleSelectItem={handleSelectItem} />
-      <Modal
-        style={{ margin: 0, backgroundColor: '#FFF' }}
-        statusBarTranslucent
-        animationIn="fadeIn"
-        isVisible={state.modal}
-        onBackButtonPress={() => setState({ ...state, modal: false })}>
-          <ModalScreen progress={progress} state={state} handleDownload={handleDownload} />
-      </Modal>
+      <Masonry screen='data' data={media} handleSelectItem={handleSelectItem} />
+      <ModalScreen
+        data={media}
+        state={state}
+        onBackButtonPress={() => setState({ ...state, modal: false })}
+      />
     </Styles.Container>
   );
 };
