e5445b2c4cf9fd2f2ce89f199887a85549bb9255
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-09-04 11:24:34 -0300

    creating a download for files

--- src/common/video/index.tsx
+++ src/common/video/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as Native from 'react-native';
 import * as ExpoAv from 'expo-av';
+import * as Styles from './styles';
+import { Icon } from '../svg';
+
+import { useDownloads } from '@hooks';
 
 const {width: WIDTH_FOR_IMAGE, height: HEIGHT_FOR_IMAGE} = Native.Dimensions.get('window');
 
-export const Video = React.forwardRef(({ posts, refScroll }: any, parentRef) => {
+export const Video = React.forwardRef(({ posts }: any, parentRef) => {
   const ref = React.useRef<any>(null);
-  React.useImperativeHandle(parentRef, () => ({ play, stop, unload }));
+  const [progress, setProgress] = React.useState<'download' | 'loading' | 'check'>(posts.downloaded ? 'check' : 'download');
+  const [showOptions, setShowOptions] = React.useState<boolean>(false);
 
+  React.useImperativeHandle(parentRef, () => ({ play, stop, unload }));
   React.useEffect(() => {
     return function(){unload()}
   }, []);
@@ -15,7 +21,6 @@ export const Video = React.forwardRef(({ posts, refScroll }: any, parentRef) => 
   const play = async() => {
     if (ref.current == null) return;
 
-    // if video is already playing return
     const status = await ref.current.getStatusAsync();
     if (status?.isPlaying) return;
 
@@ -27,44 +32,79 @@ export const Video = React.forwardRef(({ posts, refScroll }: any, parentRef) => 
   };
 
   const stop = async() => {
-    if (ref.current == null) {
-      return;
-    };
+    if (ref.current == null) return;
 
-    // if video is already stopped return
     const status = await ref.current.getStatusAsync();
     if (!status?.isPlaying) return;
+
+    setShowOptions(false);
 
     try {
       await ref.current.stopAsync();
     } catch (e) {
       console.log(e)
-    }
+    };
   };
 
   const unload = async() => {
-    // console.log('unload');
     if (ref.current == null) return;
 
-    // if video is already stopped return
     try {
       await ref.current.unloadAsync();
     } catch (e) {
       console.log(e)
-  }
+    };
+  };
+
+  const handleDownloadVideo = () => {
+    setProgress('loading')
+    useDownloads({
+      folder: 'Storage',
+      url:posts.url,
+      creator:`${posts.id}_${posts.createby}`,
+      mediaType:posts.mediaType === 'video' ? '.mp4' : '.png'
+    })
+      .then(() => setProgress('check'))
+      .catch(() => setProgress('download'))
+  };
+
+  const handleIsPlaying = async() => {
+    if(posts.mediaType === 'photo'){
+      return setShowOptions(!showOptions);
+    };
+
+    const status = await ref.current.getStatusAsync();
+    if (status?.isPlaying) {
+      await ref.current.pauseAsync();
+      setShowOptions(true);
+    } else {
+      await ref.current.playAsync();
+      setShowOptions(false);
+    }
   };
 
   return (
-    <ExpoAv.Video
-      ref={ref}
-      shouldPlay={false}
-      isLooping
-      usePoster
-      posterSource={{ uri: posts.url }}
-      source={{ uri: posts.url }}
-      style={{ flex: 1, width: WIDTH_FOR_IMAGE, height: HEIGHT_FOR_IMAGE }}
-      resizeMode={ExpoAv.ResizeMode.CONTAIN}
-    />
+    <Styles.Container activeOpacity={1} onPress={handleIsPlaying}>
+      <ExpoAv.Video
+        ref={ref}
+        isLooping
+        usePoster
+        shouldPlay={false}
+        source={{ uri: posts.url || posts.uri }}
+        posterSource={{ uri: posts.url || posts.uri }}
+        resizeMode={ExpoAv.ResizeMode.CONTAIN}
+        style={{ flex: 1, width: WIDTH_FOR_IMAGE, height: HEIGHT_FOR_IMAGE }}
+      />
+      <Styles.Actions display={showOptions} width={posts.width} height={posts.height}>
+        <Native.TouchableOpacity
+          style={{ display: posts.downloaded !== undefined ? 'flex' : 'none' }}
+          onPress={handleDownloadVideo}
+          disabled={progress === 'check' || posts.downloaded}
+        >
+          <Icon name={progress} />
+        </Native.TouchableOpacity>
+      </Styles.Actions>
+    </Styles.Container>
   );
 });
 
--- src/common/video/styles.ts
+++ src/common/video/styles.ts
@@ -1,5 +1,21 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
-  
+export const Container = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Actions = styled.View`
+  ${({ display, width, height }: { display: boolean, width: string, height:string }) => css`
+    display: ${display ? 'flex' : 'none'};
+    width: ${width}px;
+    height: ${height}px;
+  `};
+
+  flex-direction: row;
+  position: absolute;
+  align-items: center;
+  justify-content: center;
+  background-color: #5050505a;
 `;
